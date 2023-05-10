95f840e3defc3f074a3e765f2af2b0e6749e784c
amotta <andrea10.motta@mail.polimi.it>
2023-05-10 09:38:51 +0200

    aes trace spoofing

--- Demo/RISC-V-Qemu-virt_GCC/main_aes.c
+++ Demo/RISC-V-Qemu-virt_GCC/main_aes.c
@@ -36,12 +36,14 @@
 #include "riscv-virt.h"
 
 /* Priorities used by the tasks. */
-#define mainQUEUE_RECEIVE_TASK_PRIORITY		( tskIDLE_PRIORITY + 2)
-#define	mainQUEUE_SEND_TASK_PRIORITY		( tskIDLE_PRIORITY + 1)
+#define mainQUEUE_RECEIVE_TASK_PRIORITY (tskIDLE_PRIORITY + 2)
+#define mainQUEUE_SEND_TASK_PRIORITY (tskIDLE_PRIORITY + 1)
 
+#define mainAES_TASK_PRIOTIY (tskIDLE_PRIORITY + 1)
+#define mainPRIO_MANAGER_TASK_PRIOTIY (tskIDLE_PRIORITY + 2)
 /* The rate at which data is sent to the queue.  The 200ms value is converted
 to ticks using the pdMS_TO_TICKS() macro. */
-#define mainQUEUE_SEND_FREQUENCY_MS			pdMS_TO_TICKS( 1000 )
+#define mainQUEUE_SEND_FREQUENCY_MS pdMS_TO_TICKS(1000)
 
 /* The maximum number items the queue can hold.  The priority of the receiving
 task is above the priority of the sending task, so the receiving task will
@@ -49,205 +51,159 @@ preempt the sending task and remove the queue items each time the sending task
 writes to the queue.  Therefore the queue will never have more than one item in
 it at any time, and even with a queue length of 1, the sending task will never
 find the queue full. */
-#define mainQUEUE_LENGTH					( 1 )
+#define mainQUEUE_LENGTH (1)
+#define aesFAKE_COUNT (1)
+#define init_state                                                                                     \
+	{                                                                                                  \
+		0x40, 0x41, 0x40, 0x41, 0x40, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41 \
+	}
+#define true_key                                                                                       \
+	{                                                                                                  \
+		0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c \
+	}
+#define puf_key                                                                                        \
+	{                                                                                                  \
+		0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c \
+	}
+
+typedef struct aes_param
+{
+	uint8_t tag;
+	uint8_t *key;
+	uint8_t *state;
+	TaskHandle_t *task;
+} aes_param_t;
+
+uint8_t states[aesFAKE_COUNT +1][16];
+uint8_t keys[aesFAKE_COUNT +1][16];
+aes_param_t aes_CB[aesFAKE_COUNT + 1];
 
 /* The queue used by both tasks. */
 static QueueHandle_t xAesInQueue = NULL;
 static QueueHandle_t xFakeAesInQueue = NULL;
 
-/*--AES-testing--------------------------Vendors/aes.c-------*/
-uint8_t state[16] ={0x40,0x41,0x40,0x41,0x40,0x41,0x41,0x41,0x41,0x41,0x41,0x41,0x41,0x41,0x41,0x41,};
-uint8_t fake_state[16] ={0x40,0x41,0x40,0x41,0x40,0x41,0x41,0x41,0x41,0x41,0x41,0x41,0x41,0x41,0x41,0x41,};
-
-uint8_t key[16] = {0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c};
-uint8_t fake_key[16] = {0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c};
-uint8_t puf_key[16] = {0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c};
-
-extern int aes_run(uint8_t*, uint8_t*);
+extern int aes_run(uint8_t *state, uint8_t *key);
 /*-----------------------------------------------------------*/
+
+static void prvPriorityManagerTask(void *pvParameters)
+{
+	aes_param_t *aes_current_cb = (aes_param_t *) pvParameters;
+	uint8_t selector = 0;
+	while (1)
+	{	
+		portENTER_CRITICAL()
+		selector += 1;
+		selector = selector % (aesFAKE_COUNT +1 );
+		for (uint8_t i = 0; i < (aesFAKE_COUNT + 1); i++)
+		{
+			TaskHandle_t aes_selected_handler = *aes_current_cb[i].task;
+			if (i != selector)
+			{
+				vTaskPrioritySet(aes_selected_handler, mainAES_TASK_PRIOTIY);
+			}
+			else{
+				vTaskPrioritySet(aes_selected_handler, mainPRIO_MANAGER_TASK_PRIOTIY);
+			}
+		}
+		portEXIT_CRITICAL()
+		taskYIELD()
+	}
+}
+
+static void prvEncoderTask(void *pvParameters)
+{
+	aes_param_t *aes_current_cb = (aes_param_t *) pvParameters;
+
+	while (1)
+	{
+		char buf[40];
+		char aes_buf[4 + (2 * 16)];
+		aes_run(aes_current_cb->state, aes_current_cb->key);
+		for (uint8_t i = 0; i < 16; i++)
+		{
+			sprintf(aes_buf + (4 + (2 * i)), "%02x", aes_current_cb->state[i]);
+		}
+#ifdef QEMU
+			sprintf(aes_buf, "AES%d:", aes_current_cb->tag);
+			vSendString(buf);
+#endif
+	}
+}
 
 /*
-* Blocks dispatcher: put a block address in xAesInQueue
-*/
-static void prvDispatcherTask( void *pvParameters )
+ * AES environment setup, generates necessary aes states and keys:
+ *	a state and key pair for the real aes
+ *	aesFAKE_COUNT state and key pairs for the fake ones, each key is generated running a puf N times,
+ *	with N being different for each key
+ */
+static void aesSetup()
 {
-	uint8_t *state_address = state;
-	uint8_t *fake_state_address = fake_state;
 
-	TickType_t xNextWakeTime;
-	const char * const pcMessage = "Block enqueued";
-	char buf[40];
+	uint8_t temp_puf[16] = puf_key;
 
-	/* Remove compiler warning about unused parameter. */
-	( void ) pvParameters;
-	/* Initialise xNextWakeTime - this only needs to be done once. */
-	xNextWakeTime = xTaskGetTickCount();
-
-	sprintf( buf, "%d: %s: %s", xGetCoreID(),
-				pcTaskGetName( xTaskGetCurrentTaskHandle() ),
-				pcMessage);
-		#ifdef QEMU 
-			vSendString( buf );
-		#endif
-
-	xQueueSend( xAesInQueue, &state_address , 0U );
-	xQueueSend( xFakeAesInQueue, &fake_state_address, 0U );
-
-	while(1)
+	for (uint8_t i = 0; i < (aesFAKE_COUNT + 1); i++)
 	{
-		sprintf( buf, "%d: %s: %s", xGetCoreID(),
-				pcTaskGetName( xTaskGetCurrentTaskHandle() ),
-				pcMessage);
-		#ifdef QEMU 
-			vSendString( buf );
-		#endif
+		uint8_t temp[16] = init_state;
+		uint8_t temp_key[16] = true_key;
 
-		/* Place this task in the blocked state until it is time to run again. */
-		xTaskDelayUntil( &xNextWakeTime, mainQUEUE_SEND_FREQUENCY_MS );
+		for (uint8_t j = 0; j < 16; j++)
+		{
+			states[i][j] = temp[j];
+			keys[i][j] = temp_key[j];
+		}
 
-		/* Send to the queue - causing the queue receive task to unblock and
-		toggle the LED.  0 is used as the block time so the sending operation
-		will not block - it shouldn't need to block as the queue should always
-		be empty at this point in the code. */
-		xQueueSend( xAesInQueue, &state_address, 0U );
-		xQueueSend( xFakeAesInQueue, &state_address, 0U );
+		if (i != 0)
+		{
+			for (uint8_t k = 0; k < i; k++)
+			{
+				aes_run(keys[i], temp_puf);
+			}
+		}
 	}
 }
-
 /*-----------------------------------------------------------*/
 
-static void prvEncoderTask( void *pvParameters )
+int main_aes(void)
 {
-	uint8_t *receivedState;
-	const char * const pcFailMessage = "Unexpected value received\r\n";
-	/* Remove compiler warning about unused parameter. */
-	( void ) pvParameters;
-
-	while(1)
+#ifdef QEMU
+	vSendString("FreeRTOS AES QEMU dev bench:");
+	vSendString("AES setup start");
+#endif	
+	aesSetup();
+#ifdef QEMU
+	vSendString("AES setup completed");
+	vSendString("Tasks setup start");
+#endif	
+	for (uint8_t i = 0; i < (aesFAKE_COUNT + 1); i++)
 	{
-		char buf[40];
-		char aes_buf[4+(2*16)];
-		sprintf(aes_buf,"AES:");
-		/* Wait until something arrives in the queue - this task will block
-		indefinitely provided INCLUDE_vTaskSuspend is set to 1 in
-		FreeRTOSConfig.h. */
-		if(xQueueReceive( xAesInQueue, &receivedState, portMAX_DELAY ) == pdFALSE)
-		{
-			sprintf(buf, "task %s: failed receive from queue",
-					pcTaskGetName( xTaskGetCurrentTaskHandle()));
-			#ifdef QEMU 
-			vSendString( buf );
-			#endif
-		}
-		
-		
-		if( receivedState == state )
-		{
-			aes_run(state, key);
-			for (uint8_t i = 0; i < 16; i++)
-			{
-				sprintf(aes_buf+(4+(2*i)),"%02x",receivedState[i]);
-				
-			}
-			#ifdef QEMU 
-				vSendString( buf );
-			#endif
-		}
-		else
-		{
-			#ifdef QEMU 
-				vSendString( pcFailMessage );
-			#endif
-		}
-	}
-}
-
-static void prvFakeEncoderTask( void *pvParameters )
-{
-	uint8_t *receivedState;
-	const char * const pcFailMessage = "Unexpected value received\r\n";
-	/* Remove compiler warning about unused parameter. */
-	( void ) pvParameters;
-
-	while(1)
-	{
-		char buf[40];
-		char aes_buf[4+(2*16)];
-		sprintf(aes_buf,"AES:");
-		/* Wait until something arrives in the queue - this task will block
-		indefinitely provided INCLUDE_vTaskSuspend is set to 1 in
-		FreeRTOSConfig.h. */
-		if(xQueueReceive( xFakeAesInQueue, &receivedState, portMAX_DELAY ) == pdFALSE)
-		{
-			sprintf(buf, "task %s: failed receive from queue",
-					pcTaskGetName( xTaskGetCurrentTaskHandle()));
-			#ifdef QEMU 
-				vSendString( buf );
-			#endif
-		}
-		
-		
-		if( receivedState == state )
-		{
-			for (uint8_t i = 0; i < 16; i++)
-			{
-				fake_state[i] = receivedState[i];
-			}
-			aes_run(fake_state, fake_key);
-			for (uint8_t i = 0; i < 16; i++)
-			{
-				sprintf(aes_buf+(4+(2*i)),"%02x",fake_state[i]);
-				
-			}
-			#ifdef QEMU 
-				vSendString( buf );
-			#endif
-		}
-		else
-		{
-			#ifdef QEMU 
-				vSendString( pcFailMessage );
-			#endif
-		}
-	}
-}
-
-/*-----------------------------------------------------------*/
-
-int main_aes( void )
-{
-	#ifdef QEMU 
-		vSendString( "FreeRTOS AES QEMU dev bench:" );
-		vSendString( "Tasks create start" );
-	#endif
-	
-	
-	/* Create the queue. */
-	xAesInQueue = xQueueCreate( mainQUEUE_LENGTH, sizeof( uint8_t* ) );
-	xFakeAesInQueue = xQueueCreate( mainQUEUE_LENGTH, sizeof( uint8_t* ) );
-	
-	/* Encode the fake key from the true key with a puf function*/
-	aes_run(fake_key,puf_key);
-
-	if( xAesInQueue != NULL )
-	{
-		/* Start the two tasks as described in the comments at the top of this
-		file. */
-		xTaskCreate( prvEncoderTask, "AES", configMINIMAL_STACK_SIZE * 2U, NULL,
-					mainQUEUE_RECEIVE_TASK_PRIORITY, NULL );
-		xTaskCreate( prvFakeEncoderTask, "FAKEAES", configMINIMAL_STACK_SIZE * 2U, NULL,
-					mainQUEUE_RECEIVE_TASK_PRIORITY, NULL );
-		xTaskCreate( prvDispatcherTask, "Tx", configMINIMAL_STACK_SIZE * 2U, NULL,
-					mainQUEUE_SEND_TASK_PRIORITY, NULL );
-		#ifdef QEMU			
-		//SendString( "Tasks create success" );
+		aes_CB[i].tag = i;
+		aes_CB[i].key = keys[i];
+		aes_CB[i].state = states[i];
+		xTaskCreate(	
+			prvEncoderTask, 
+			"AES", 
+			configMINIMAL_STACK_SIZE * 2U, 
+			(void *) &(aes_CB[i]),
+			mainAES_TASK_PRIOTIY, 
+			aes_CB[i].task
+		);
+		#ifdef QEMU
+			char buf[30];
+			sprintf(buf, "Tasks %d created", i);
 		#endif
 	}
-	#ifdef QEMU
-	//vSendString( "Scheduler started" );
-	#endif
+	xTaskCreate(	
+		prvPriorityManagerTask, 
+		"PRIORITY MANAGER", 
+		configMINIMAL_STACK_SIZE * 2U, 
+		(void *) aes_CB,
+		mainPRIO_MANAGER_TASK_PRIOTIY, 
+		NULL
+	);
+#ifdef QEMU
+	vSendString( "Tasks setup completed" );
+	vSendString( "Scheduler started" );
+#endif
 	vTaskStartScheduler();
-	//vSendString( "Failed to start scheduler" );
-
 	return 0;
 }
